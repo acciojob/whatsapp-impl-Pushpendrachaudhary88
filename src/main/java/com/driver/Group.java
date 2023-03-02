d22f89723962c658b3cf6f96c556d45017299558
Pushpendrachaudhary88 <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-03-03 00:07:54 +0530

    WhatsApp Assginment Sol

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -4,16 +4,11 @@ public class Group {
     private String name;
     private int numberOfParticipants;
 
+    public Group() {
+    }
+
     public Group(String name, int numberOfParticipants) {
         this.name = name;
-        this.numberOfParticipants = numberOfParticipants;
-    }
-
-    public int getNumberOfParticipants() {
-        return numberOfParticipants;
-    }
-
-    public void setNumberOfParticipants(int numberOfParticipants) {
         this.numberOfParticipants = numberOfParticipants;
     }
 
@@ -23,5 +18,13 @@ public class Group {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
     }
 }
